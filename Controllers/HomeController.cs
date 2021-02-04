d8f64240ba3d323d263b33096ece08add04e8a1a
mbhayden14 <mbhayden14@gmail.com>
2021-02-03 19:05:51 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using JoelHiltonFilmCollection.Models;
+
+namespace JoelHiltonFilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FilmSubmission()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FilmSubmission(ApplicationResponse applicationResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddApplication(applicationResponse);
+                return View("Confirmation", applicationResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult FilmCollection()
+        {
+            return View(TempStorage.Applications);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace JoelHiltonFilmCollection.Models
+{
+    public class ApplicationResponse
+    {
+        [Required(ErrorMessage = "Please enter a title.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1887, 2100, ErrorMessage = "Please enter a valid year.")]
+        public int Year { get; set; }
+        [Required(ErrorMessage = "Please enter a director.")]
+        public string Director { get; set; }
+        [Required(ErrorMessage = "Please enter a rating.")]
+        public string Rating { get; set; }
+        public bool? Edited { get; set; }
+        public string LentTo { get; set; }
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
